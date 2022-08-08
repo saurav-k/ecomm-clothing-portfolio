391d653875d5c6bc61c5c8acbd7e44c76b53969e
Saurav Kumar Trivedi <strivedi@fanatics.com>
2022-08-08 15:30:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return <div className="categories-container">
+    <div className="category-container">
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <h3>Shop Now</h3>
+      </div>
+    </div>
+  </div>;
+};
+
+export default App;
